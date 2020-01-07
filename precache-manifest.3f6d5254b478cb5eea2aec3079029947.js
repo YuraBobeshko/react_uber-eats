bd029e3e9ed05d0c08d6888d12e6fc7fa9a0248f
YuraBobeshko <gorobech55@gmail.com>
2020-01-07 22:18:10 +0200

    Updates

---
+++ precache-manifest.3f6d5254b478cb5eea2aec3079029947.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7669562a61c523275ca7135b5bfda6cf",
+    "url": "/react_uber-eats/index.html"
+  },
+  {
+    "revision": "2083dd26215eecb361dc",
+    "url": "/react_uber-eats/static/css/main.03d36588.chunk.css"
+  },
+  {
+    "revision": "e4f788ce533cd7b4da75",
+    "url": "/react_uber-eats/static/js/2.09746986.chunk.js"
+  },
+  {
+    "revision": "2083dd26215eecb361dc",
+    "url": "/react_uber-eats/static/js/main.fd6cbc1a.chunk.js"
+  },
+  {
+    "revision": "183d5977488a46d51899",
+    "url": "/react_uber-eats/static/js/runtime~main.bef14170.js"
+  }
+]);
